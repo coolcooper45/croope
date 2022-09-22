948d38c994514c11580b4c744bd9509da07cce0f
coolcooper45 <lazyfools1@gmail.com>
2022-09-21 20:37:18 -0500

    Cursor Movement

---
+++ Assets/Mouselook.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouselook : MonoBehaviour
+{
+
+    public float mouseSens = 2;
+    public Transform playerBody;
+    float xRotate = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X")*mouseSens*Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y")*mouseSens*Time.deltaTime;
+
+        xRotate -= mouseY;
+        xRotate = Mathf.Clamp(xRotate, -5, 45);
+
+        transform.localRotation = Quaternion.Euler(xRotate, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
